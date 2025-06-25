0bc2f30d2da54ca60e9fafa7eca3769b232d302d
PimPomUwU <daviparralopez@gmail.com>
2025-06-25 09:44:34 -0500

    Side y cositas.txt

--- resources/views/includes/sidebar/seller.blade.php
+++ resources/views/includes/sidebar/seller.blade.php
@@ -1,46 +1,36 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Test</title>
-  <link href="assets/css/main.css" rel="stylesheet">
-</head>
-<body>
-  <div>
+<div>
     <div class="border-1 w-[60%] md:w-fit h-screen">
-      <!-- Sección superior (perfil) -->
-      <div class="bg-white flex flex-col items-center p-4 ">
-        <img src="assets/images/userProfiel.svg" alt="Perfil" class="w-12 h-12 md:w-auto md:h-auto">
-        <p class="text-azulPotente text-sm md:text-lg mt-2">pedro picapiedra</p>
-        <a href="" class="text-[#4690FF] text-xs md:text-base">vendedor</a>
-        <p class="bg-[#06406A] text-white py-1 px-3 md:px-5 w-fit rounded-[16px] mt-2 md:mt-3 text-xs md:text-sm">perfil</p>
-      </div>
-      
-      <!-- Menú -->
-      <div class="bg-[#06406A] h-full">
-        <div class="p-2 md:p-4">
-          <img src="assets/images/home.svg" class="inline w-7 h-7 md:w-auto md:h-auto">
-          <p class="text-white inline text-[1.2em] ml-1 md:text-[1.3em]">Inicio</p>
+        <!-- Sección superior (perfil) -->
+        <div class="bg-white flex flex-col items-center p-4 ">
+            <img src="{{ asset('svg/icons/userProfiel.svg') }}" alt="Perfil" class="w-12 h-12 md:w-auto md:h-auto">
+            <p class="text-azulPotente text-sm md:text-lg mt-2">pedro picapiedra</p>
+            <a href="" class="text-[#4690FF] text-xs md:text-base">vendedor</a>
+            <p class="bg-[#06406A] text-white py-1 px-3 md:px-5 w-fit rounded-[16px] mt-2 md:mt-3 text-xs md:text-sm">
+                perfil</p>
         </div>
-        <div class="p-2 md:p-4">
-          <img src="assets/images/favorites.svg" class="inline w-7 h-7 md:w-auto md:h-auto">
-          <p class="text-white inline text-[1.2em] ml-1 md:text-[1.3em]">Favoritos</p>
+
+        <!-- Menú -->
+        <div class="bg-[#06406A] h-full">
+            <div class="p-2 md:p-4">
+                <img src="{{ asset('svg/icons/home.svg') }}" class="inline w-7 h-7 md:w-auto md:h-auto">
+                <p class="text-white inline text-[1.2em] ml-1 md:text-[1.3em]">Inicio</p>
+            </div>
+            <div class="p-2 md:p-4">
+                <img src="{{ asset('svg/icons/favorites.svg') }}" class="inline w-7 h-7 md:w-auto md:h-auto">
+                <p class="text-white inline text-[1.2em] ml-1 md:text-[1.3em]">Favoritos</p>
+            </div>
+            <div class="p-2 md:p-4">
+                <img src="{{ asset('svg/icons/addPublication.svg') }}" class="inline w-7 h-7 md:w-auto md:h-auto">
+                <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Publicar producto</p>
+            </div>
+            <div class="p-2 md:p-4">
+                <img src="{{ asset('svg/icons/messages.svg') }}" class="inline w-7 h-7 md:w-auto md:h-auto">
+                <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Mensajes</p>
+            </div>
+            <div class="p-2 md:p-4">
+                <img src="{{ asset('svg/icons/support.svg') }}" class="inline w-7 h-7 md:w-auto md:h-auto">
+                <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Soporte y preguntas frecuentes</p>
+            </div>
         </div>
-        <div class="p-2 md:p-4">
-          <img src="assets/images/addPublication.svg" class="inline w-7 h-7 md:w-auto md:h-auto">
-          <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Publicar producto</p>
-        </div>
-        <div class="p-2 md:p-4">
-          <img src="assets/images/messages.svg" class="inline w-7 h-7 md:w-auto md:h-auto">
-          <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Mensajes</p>
-        </div>
-        <div class="p-2 md:p-4">
-          <img src="assets/images/support.svg" class="inline w-7 h-7 md:w-auto md:h-auto">
-          <p class="text-white inline  text-[1.2em] ml-1 md:text-[1.3em]">Soporte y preguntas frecuentes</p>
-        </div>
-      </div>
     </div>
-  </div>
-</body>
-</html>
+</div>
