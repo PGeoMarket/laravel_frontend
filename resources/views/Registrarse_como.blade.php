b342859cf5a330628af9fd8148011e669746af40
Stiven <almeidaibarra.stiven@gmail.com>
2025-07-01 13:00:06 -0500

    refactor: clean up HTML structure and improve CSS classes in Registrarse_como view

--- resources/views/Registrarse_como.blade.php
+++ resources/views/Registrarse_como.blade.php
@@ -1,26 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
+
+<body class="bg-black">
     <img src="{{ asset('svg/icons/back-icon.svg') }}" alt="Volver" class="cursor-pointer">
     <div class="">
 
-        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center mb-6 cursor-pointer">
+        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center mb-6 cursor-pointer display-flex">
             <img src="{{ asset('svg/icons/store.svg') }}" alt="Vendedor" class="w-12 mb-3">
             <span class="text-white text-lg">Vendedor</span>
         </div>
 
-        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center cursor-pointer">
-            <img src="{{ asset('svg/icons/cart.svg') }}" alt="Consumidor" class="w-12 mb-3">
+        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center cursor-pointer display-flex">
+            <img src="{{ asset('svg/icons/cart.svg') }}" alt="Consumidor" class="w-12 mb-3 cursor-pointer">
             <span class="text-white text-lg">Consumidor</span>
         </div>
     </div>
 </body>
 
-</html>
+
