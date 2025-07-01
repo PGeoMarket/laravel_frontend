06fc16352fbe00fc4334f669ab6f8b1544c92ab7
Stiven <almeidaibarra.stiven@gmail.com>
2025-07-01 12:22:36 -0500

    a

--- resources/views/Registrarse_como.blade.php
+++ resources/views/Registrarse_como.blade.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+    <img src="{{ asset('svg/icons/back-icon.svg') }}" alt="Volver" class="cursor-pointer">
+    <div class="">
 
-
-        <img src="{{ asset('svg/icons/back-icon.svg') }}" alt="">
-        <div class="ml-4">
-            <img src="{{ asset('svg/icons/cart.svg') }}" alt="">
+        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center mb-6 cursor-pointer">
+            <img src="{{ asset('svg/icons/store.svg') }}" alt="Vendedor" class="w-12 mb-3">
+            <span class="text-white text-lg">Vendedor</span>
         </div>
-  
-    
+
+        <div class="bg-[#06406A] rounded-xl w-44 h-36 items-center justify-center cursor-pointer">
+            <img src="{{ asset('svg/icons/cart.svg') }}" alt="Consumidor" class="w-12 mb-3">
+            <span class="text-white text-lg">Consumidor</span>
+        </div>
+    </div>
 </body>
+
 </html>
