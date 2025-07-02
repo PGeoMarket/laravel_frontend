08e89b5dbedcfb7198074eaba6da4f7566714b16
JuanDiPosada <juandiegocifuentesposada@gmail.com>
2025-07-01 19:33:14 -0500

    fix: el anterior commit hacia modificaciones en app.blade que impedian el funcionamiento normal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @include('includes.navbar')
     </header>
     {{-- no tocar ese pt-16 --}}
-    <main class="pt-15">
+    <main class="pt-18 p-2">
         @yield('content')
     </main>
 
